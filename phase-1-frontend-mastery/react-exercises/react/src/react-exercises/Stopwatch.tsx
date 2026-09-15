4cdd7e6f7078a15a305286860f45076a909f62aa
Victoria Coquet <vcoquet@xpansiv.com>
2026-09-15 16:29:37 -0400

    Build stopwatch

--- phase-1-frontend-mastery/react-exercises/react/src/react-exercises/Stopwatch.tsx
+++ phase-1-frontend-mastery/react-exercises/react/src/react-exercises/Stopwatch.tsx
@@ -1,26 +1,38 @@
 import { useRef, useState } from "react";
 
+let laps: [number, string][] = [];
+let [milliseconds, seconds, minutes, hours] = [0, 0, 0, 0];
+
+function formatTime(time: number): string {
+	return time < 10 ? "0" + time : time.toString();
+}
+
+function Lap({ lap }: { lap: [number, string] }) {
+	return (
+		<div className="text-white w-full flex justify-between text-sm border-b">
+			<div>Lap #{lap[0]}</div>
+			<div>{lap[1]}</div>
+		</div>
+	)
+}
+
 export default function StopwatchPage() {
-	let [milliseconds, seconds, minutes, hours] = [0, 0, 0, 0];
 	let [displayTime, setDisplayTime] = useState('00:00:00.00');
 	let [started, setStarted] = useState(false);
+	let [lapNo, setLapNo] = useState(0);
 	let timer = useRef<number>(0);
-
-	function formatTime(time: number): string {
-		return time < 10 ? "0" + time : time.toString();
-	}
 
 	function stopwatch() {
 		milliseconds++;
 		if (milliseconds === 100) {
 			milliseconds = 0;
-			seconds++;
+			seconds++
 			if (seconds === 60) {
-				seconds = 0;
+				seconds = 0
 				minutes++;
 				if (minutes === 60) {
-					minutes = 0;
-					hours++;
+					minutes = 0
+					hours++
 				}
 			}
 		}
@@ -32,7 +44,7 @@ export default function StopwatchPage() {
 		setDisplayTime(`${h}:${m}:${s}.${ms}`);
 	}
 
-	function watchStartStop() {
+	function startOrStop() {
 		if (started) {
 			clearInterval(timer.current);
 			setStarted(false);
@@ -42,12 +54,30 @@ export default function StopwatchPage() {
 		}
 	}
 
+	function resetOrLap() {
+		if (started) {
+			setLapNo(lapNo = lapNo + 1);
+			laps.push([lapNo, displayTime])
+		} else {
+			clearInterval(timer.current);
+			[milliseconds, seconds, minutes, hours] = [0, 0, 0, 0]
+			setDisplayTime('00:00:00.00')
+			setLapNo(0);
+			laps = []
+		}
+	}
+
 	return (
-		<div className="w-full flex flex-col items-center gap-8">
+		<div className="flex flex-col items-center gap-8 w-fit mx-auto">
 			<div className="text-7xl font-bold text-white mt-20 font-mono">{displayTime}</div>
-			<div className="flex items-center gap-4 *:bg-white *:py-2 *:px-5 *:rounded-md *:text-gray-600">
-				<button onClick={watchStartStop}>{!started ? 'Start' : 'Stop'}</button>
-				<button>{started ? 'Lap' : 'Reset'}</button>
+			<div className="flex items-center gap-4 *:py-2 *:px-5 *:rounded-md *:text-white *:w-30">
+				<button onClick={startOrStop} className={!started ? "bg-green-400" : "bg-red-400"}>{!started ? 'Start' : 'Stop'}</button>
+				<button onClick={resetOrLap} className={"bg-gray-400"}>{started ? 'Lap' : 'Reset'}</button>
+			</div>
+			<div className="w-full *:py-2">
+				{laps.map((lap) =>
+					<Lap lap={lap} key={lap[0]} />
+				)}
 			</div>
 		</div>
 	)
